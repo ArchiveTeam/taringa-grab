364c58190a881db945e397d1fab9139261fc1219
arkiver <arkiver@protonmail.com>
2024-03-20 10:40:32 +0100

    Version 20240320.06. Do not run 200 content check on media.taringa.net URLs.

--- taringa.lua
+++ taringa.lua
@@ -575,7 +575,8 @@ wget.callbacks.write_to_warc = function(url, http_stat)
       return false
     end
     local json = cjson.decode(percent_encode_url(decode_codepoint(html)))
-  elseif string.match(url["url"], "^https?://[^/]*taringa%.net/") then
+  elseif string.match(url["url"], "^https?://taringa%.net/")
+    or string.match(url["url"], "^https?://www%.taringa%.net/") then
     local html = read_file(http_stat["local_file"])
     if not string.match(html, '"datePublished"')
       or not string.match(html, '"author"') then
